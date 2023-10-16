b2a28aba39930cc79ae812f39360bf0dfecbadcf
leonardogr15 <lgr456@hotmail.es>
2023-10-16 09:18:45 -0500

    add waitrees

--- backend.py
+++ backend.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify,render_template
 import socket
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -42,4 +43,5 @@ def check_internet_connection():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True, port=1221)
+	serve(app,host='0.0.0.0',port=8000)
+    #app.run(host='0.0.0.0',debug=True, port=1221)
